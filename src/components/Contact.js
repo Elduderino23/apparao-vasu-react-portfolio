64f7f3b3447ff051321392598556f4ef43f8520c
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-12-04 18:50:07 -0800

    fix contact form to validate email and add helper.js file and utils folder

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,22 +12,22 @@ const styles = {
     color: "gold",
     textShadow: "2px 2px 1px black, 0 0 25px maroon, 0 0 5px darkblue"
   },
-
+  
 };
 const ContactForm = () => {
-  const [formState, formReport] = React.useState('Send')
+  const [email, setEmail] = useState('');
+  const [formState] = useState('Send')
   const [errorMessage, setErrorMessage] = useState('');
-  const testThis = (e) => {
-    e.preventDefault()
-    formReport('Please wait while your input submit')
-    const { name, email, message } = e.target.elements
-    let subForm = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    }
-    // const [email, setEmail] = useState('');
-  }
+  // const testThis = (e) => {
+  //   e.preventDefault()
+  //   formReport('Please wait while your input submit')
+  //   const { name, email, message } = e.target.elements
+  //   let subForm = {
+  //     name: name.value,
+  //     email: email.value,
+  //     message: message.value,
+  //   }
+  // }
     const handleInputChange = (e) => {
       const { target } = e;
       const inputType = target.name;
@@ -47,10 +47,11 @@ const ContactForm = () => {
         setErrorMessage('Email is invalid');
         return;
       
+      }else{
+        setErrorMessage('email submitted')
+        return
       }
   
-      setEmail('');
-    console.log(subForm)
   }
 
   
@@ -58,21 +59,21 @@ const ContactForm = () => {
    <section style={styles.contactStyles}>
      <div className="container mt-5">
       <h2 className="mb-3 ContactMe"style={styles.textStyle}>Contact</h2>
-      <form onSubmit={testThis}>
+      <form onSubmit={handleFormSubmit}>
         <div className="mb-3">
           <label className="form-label" htmlFor="name" style={styles.textStyle}>
             Name
           </label>
-          <input className="form-control" type="email" id="name" required value={email}
-          name="email"
-          onChange={handleInputChange}
-          placeholder="email" />
+          <input className="form-control" type="text" id="name"/>
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="email" style={styles.textStyle}>
             Email
           </label>
-          <input className="form-control" type="email" id="email" required />
+          <input className="form-control" type="email" id="email" required value={email}
+          name="email"
+          onChange={handleInputChange}
+          placeholder="email" />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="message" style={styles.textStyle}>
